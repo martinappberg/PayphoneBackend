8fac73702e165c584c20dff3a8e3c73201828657
Martin <mkjberg@gmail.com>
2016-11-10 20:13:01 +0100

    Payment
    New

--- createpayment.js
+++ createpayment.js
@@ -1,8 +1,15 @@
-var admin = require("firebase-admin");
+  // Set the configuration for your app
+  // TODO: Replace with your project's config object
+  var firebase = require("firebase");
+  var config = {
+    apiKey: "AIzaSyBp5xOVikCrlQBK0pxfU43Me2qy4zkMQh4",
+    authDomain: "payphone-f4a67.firebaseapp.com",
+    databaseURL: "https://payphone-f4a67.firebaseio.com",
+    storageBucket: "payphone-f4a67.appspot.com",
+    messagingSenderId: "202102323605"
+  };
+  firebase.initializeApp(config);
 
-var serviceAccount = require("path/to/serviceAccountKey.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://payphone-f4a67.firebaseio.com"
-});
+  // Get a reference to the database service
+  var database = firebase.database();
+  
